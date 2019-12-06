7f28295bd368260cf80b12cc5513642776297f2c
minhtam97 <minhtam030797@gmail.com>
2019-12-06 19:42:51 +0700

    Tam branch

---
+++ QLDACNTT_QUANLYKHO/QLDACNTT_QUANLYKHO/Stock/frmAddStock.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace QLDACNTT_QUANLYKHO.Stock
+{
+    partial class frmAddStock
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmAddStock));
+            this.layoutControl1 = new DevExpress.XtraLayout.LayoutControl();
+            this.btnSave = new DevExpress.XtraEditors.SimpleButton();
+            this.txtlocationStock = new DevExpress.XtraEditors.TextEdit();
+            this.txtnameStock = new DevExpress.XtraEditors.TextEdit();
+            this.txtidStock = new DevExpress.XtraEditors.TextEdit();
+            this.Root = new DevExpress.XtraLayout.LayoutControlGroup();
+            this.layoutControlItem1 = new DevExpress.XtraLayout.LayoutControlItem();
+            this.layoutControlItem3 = new DevExpress.XtraLayout.LayoutControlItem();
+            this.layoutControlItem2 = new DevExpress.XtraLayout.LayoutControlItem();
+            this.layoutControlItem5 = new DevExpress.XtraLayout.LayoutControlItem();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControl1)).BeginInit();
+            this.layoutControl1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.txtlocationStock.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtnameStock.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtidStock.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Root)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem5)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // layoutControl1
+            // 
+            this.layoutControl1.Controls.Add(this.btnSave);
+            this.layoutControl1.Controls.Add(this.txtlocationStock);
+            this.layoutControl1.Controls.Add(this.txtnameStock);
+            this.layoutControl1.Controls.Add(this.txtidStock);
+            this.layoutControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.layoutControl1.Location = new System.Drawing.Point(0, 0);
+            this.layoutControl1.Name = "layoutControl1";
+            this.layoutControl1.Root = this.Root;
+            this.layoutControl1.Size = new System.Drawing.Size(305, 138);
+            this.layoutControl1.TabIndex = 1;
+            this.layoutControl1.Text = "layoutControl1";
+            // 
+            // btnSave
+            // 
+            this.btnSave.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("btnSave.ImageOptions.Image")));
+            this.btnSave.ImageOptions.ImageToTextAlignment = DevExpress.XtraEditors.ImageAlignToText.LeftCenter;
+            this.btnSave.Location = new System.Drawing.Point(12, 84);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(281, 38);
+            this.btnSave.StyleController = this.layoutControl1;
+            this.btnSave.TabIndex = 4;
+            this.btnSave.Text = "Lưu";
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // txtlocationStock
+            // 
+            this.txtlocationStock.Location = new System.Drawing.Point(56, 60);
+            this.txtlocationStock.Name = "txtlocationStock";
+            this.txtlocationStock.Size = new System.Drawing.Size(237, 20);
+            this.txtlocationStock.StyleController = this.layoutControl1;
+            this.txtlocationStock.TabIndex = 6;
+            // 
+            // txtnameStock
+            // 
+            this.txtnameStock.Location = new System.Drawing.Point(56, 36);
+            this.txtnameStock.Name = "txtnameStock";
+            this.txtnameStock.Size = new System.Drawing.Size(237, 20);
+            this.txtnameStock.StyleController = this.layoutControl1;
+            this.txtnameStock.TabIndex = 5;
+            // 
+            // txtidStock
+            // 
+            this.txtidStock.Location = new System.Drawing.Point(56, 12);
+            this.txtidStock.Name = "txtidStock";
+            this.txtidStock.Size = new System.Drawing.Size(237, 20);
+            this.txtidStock.StyleController = this.layoutControl1;
+            this.txtidStock.TabIndex = 4;
+            // 
+            // Root
+            // 
+            this.Root.EnableIndentsWithoutBorders = DevExpress.Utils.DefaultBoolean.True;
+            this.Root.GroupBordersVisible = false;
+            this.Root.Items.AddRange(new DevExpress.XtraLayout.BaseLayoutItem[] {
+            this.layoutControlItem1,
+            this.layoutControlItem3,
+            this.layoutControlItem2,
+            this.layoutControlItem5});
+            this.Root.Name = "Root";
+            this.Root.Size = new System.Drawing.Size(305, 138);
+            this.Root.TextVisible = false;
+            // 
+            // layoutControlItem1
+            // 
+            this.layoutControlItem1.Control = this.txtidStock;
+            this.layoutControlItem1.Location = new System.Drawing.Point(0, 0);
+            this.layoutControlItem1.Name = "layoutControlItem1";
+            this.layoutControlItem1.Size = new System.Drawing.Size(285, 24);
+            this.layoutControlItem1.Text = "Mã kho";
+            this.layoutControlItem1.TextSize = new System.Drawing.Size(41, 13);
+            // 
+            // layoutControlItem3
+            // 
+            this.layoutControlItem3.Control = this.txtlocationStock;
+            this.layoutControlItem3.Location = new System.Drawing.Point(0, 48);
+            this.layoutControlItem3.Name = "layoutControlItem3";
+            this.layoutControlItem3.Size = new System.Drawing.Size(285, 24);
+            this.layoutControlItem3.Text = "Vị trí kho";
+            this.layoutControlItem3.TextSize = new System.Drawing.Size(41, 13);
+            // 
+            // layoutControlItem2
+            // 
+            this.layoutControlItem2.Control = this.txtnameStock;
+            this.layoutControlItem2.Location = new System.Drawing.Point(0, 24);
+            this.layoutControlItem2.Name = "layoutControlItem2";
+            this.layoutControlItem2.Size = new System.Drawing.Size(285, 24);
+            this.layoutControlItem2.Text = "Tên kho";
+            this.layoutControlItem2.TextSize = new System.Drawing.Size(41, 13);
+            // 
+            // layoutControlItem5
+            // 
+            this.layoutControlItem5.Control = this.btnSave;
+            this.layoutControlItem5.Location = new System.Drawing.Point(0, 72);
+            this.layoutControlItem5.Name = "layoutControlItem5";
+            this.layoutControlItem5.Size = new System.Drawing.Size(285, 46);
+            this.layoutControlItem5.TextSize = new System.Drawing.Size(0, 0);
+            this.layoutControlItem5.TextVisible = false;
+            // 
+            // frmAddStock
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(305, 138);
+            this.Controls.Add(this.layoutControl1);
+            this.Name = "frmAddStock";
+            this.Text = "Cập nhật kho";
+            this.Load += new System.EventHandler(this.frmAddStock_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControl1)).EndInit();
+            this.layoutControl1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.txtlocationStock.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtnameStock.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtidStock.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Root)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem5)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private DevExpress.XtraLayout.LayoutControl layoutControl1;
+        private DevExpress.XtraEditors.TextEdit txtlocationStock;
+        private DevExpress.XtraEditors.TextEdit txtnameStock;
+        private DevExpress.XtraEditors.TextEdit txtidStock;
+        private DevExpress.XtraLayout.LayoutControlGroup Root;
+        private DevExpress.XtraLayout.LayoutControlItem layoutControlItem1;
+        private DevExpress.XtraLayout.LayoutControlItem layoutControlItem3;
+        private DevExpress.XtraLayout.LayoutControlItem layoutControlItem2;
+        private DevExpress.XtraEditors.SimpleButton btnSave;
+        private DevExpress.XtraLayout.LayoutControlItem layoutControlItem5;
+    }
+}
